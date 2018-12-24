5c55e220cb0836d6c160edb8820453fac2efd370
YutakaYamasaki <uminchuvirtual@gmail.com>
2018-12-24 17:34:41 +0800

    episode 4-2

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,9 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title></title>
-</head>
-<body>
+@extends('layout')
+
+@section('title','Contact Us')
+
+@section('content')
     <h1>Contact form</h1>
-</body>
-</html>
+@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title></title>
-</head>
-<body>
-    <h1>here we go!!</h1>
-    <p>
-        <a href="/contact">Contact</a> us to learn more 
-    </p>
-</body>
-</html>
+@extends('layout')
+
+@section('content')
+    <h1>My first website</h1>
+    
+@endsection
