9655a208ad5e055a0eb0f7f86748552a74c8c04f
YutakaYamasaki <uminchuvirtual@gmail.com>
2018-12-24 17:14:30 +0800

    episode 4

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title></title>
+</head>
+<body>
+    <h1>Contact form</h1>
+</body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,5 +5,8 @@
 </head>
 <body>
     <h1>here we go!!</h1>
+    <p>
+        <a href="/contact">Contact</a> us to learn more 
+    </p>
 </body>
 </html>
